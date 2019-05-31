78a0dcc41ab705cc52ab518d9177407989b27540
dewatch <2691935858@qq.com>
2019-05-31 22:46:09 +0800

    init

---
+++ resources/mongo.js
@@ -0,0 +1,59 @@
+
+let insert_many = (db, collection, data, callback) => {
+    db.collection(collection).insertMany(data, (err, result) => {
+        if(err){
+            console.log(err);
+        }else{
+            callback(result);
+        }
+    })
+}
+
+let delete_one = (db, collection, data, callback) => {
+    db.collection(collection).deleteOne(data, (err, result) => {
+        if(err){
+            console.log(err);
+        }else{
+            callback(result);
+        }
+    })
+}
+
+let update_one = (db, collection, fliter, data, callback) => {
+    db.collection(collection).updateOne(fliter, data, {upsert: false}, (err, result) => {
+        if(err){
+            console.log(err);
+        }else{
+            callback(result);
+        }
+    })
+}
+
+let find_one = (db, collection, data, callback) => {
+    db.collection(collection).findOne(data, (err, result) => {
+        if(err){
+            console.log(err);
+        }else{
+            console.log('found');
+            callback(result);
+        }
+    })
+}
+
+
+let find_all = (db, collection, data, callback) => {
+    db.collection(collection).find(data).toArray((err, result) => {
+        if(err){
+            console.log(err);
+        }else{
+            console.log('found-all');
+            callback(result);
+        }
+    })
+}
+
+module.exports.insert_many = insert_many;
+module.exports.delete_one = delete_one;
+module.exports.update_one = update_one;
+module.exports.find_one = find_one;
+module.exports.find_all = find_all;
--- resources/server.js
+++ resources/server.js
@@ -1,4 +1,21 @@
-let express = require('express');
+const express = require('express');
+const mongo = require('./mongo');
+console.log(mongo);
+const MongoClient = require('mongodb').MongoClient;
+let url = 'mongodb://localhost:27017/test';
+
 
 let server = express().listen(2333);
+
+MongoClient.connect(url, {useNewUrlParser: true}, (err, client) =>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log('connected!');
+    }
+    let db = client.db('mydb');
+    mongo.find_one(db, 'users', {username: "nsq"}, (result) => {
+        console.log(result);
+    })
+})
 
